dd669b8d806e5a6ab512919422a9f073622793a7
nazar2419 <nazarx095@gmail.com>
2020-06-20 21:19:32 +0300

    start project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+fetch('https://swapi.dev/api/people/1/')
+    .then((res) => {
+      return res.json();
+    })
+    .then((body)=> {
+      console.log(body);
+    });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
